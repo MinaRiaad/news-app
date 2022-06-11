f4f6248574bce56d74729d8a69a58f6ef4c643c4
mina <mina.riaad@gmail.com>
2022-06-11 19:05:11 +0200

    feat: add all components to app file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Banner from "src/Components/Banner";
+import Highlight from "src/Components/Highlight";
+import News from "src/Components/News";
+import Footer from "src/Components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Banner />
+      <Highlight />
+      <News />
+      <Footer />
     </div>
   );
 }
